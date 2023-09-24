685af6191628ea8ab78b660958d98849133073b1
Shaik Safi <shaiksafi2001@gmail.com>
2023-09-24 23:01:51 +0530

    backed still in progress

--- backend/quick-invoice/src/main/java/com/shaik/quickinvoice/repository/EventRepository.java
+++ backend/quick-invoice/src/main/java/com/shaik/quickinvoice/repository/EventRepository.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import com.shaik.quickinvoice.model.Client;
 import com.shaik.quickinvoice.model.Event;
 import com.shaik.quickinvoice.model.Invoice;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
+
+	List<Event> findByName(String name);
 }
--- backend/quick-invoice/src/main/java/com/shaik/quickinvoice/service/EventService.java
+++ backend/quick-invoice/src/main/java/com/shaik/quickinvoice/service/EventService.java
@@ -1,8 +1,12 @@
 package com.shaik.quickinvoice.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shaik.quickinvoice.model.Event;
 import com.shaik.quickinvoice.repository.EventRepository;
 
 @Service
@@ -10,4 +14,44 @@ public class EventService {
 
 	@Autowired
 	private EventRepository eventRepository;
+	
+	public Event add(Event event) {
+		Event savedEvent = eventRepository.save(event);
+        return savedEvent;
+    }
+    
+    public List<Event> findAll() {
+    	List<Event> savedEvents = eventRepository.findAll();
+    	return savedEvents;
+    }
+    
+    public Optional<Event> findById(Long id) {
+    	Optional<Event> event = eventRepository.findById(id);
+    	return event;
+    }
+    
+    public List<Event> findByName(String name) {
+    	List<Event> event = eventRepository.findByName(name);
+    	return event;
+    }
+    
+    public boolean existsById(Long id) {
+    	boolean eventExists = eventRepository.existsById(id);
+    	return eventExists;
+    }
+    
+    public boolean deleteById(Long id) {
+    	eventRepository.deleteById(id);
+    	return true;
+    }
+    
+    public boolean deleteEvent(Event event) {
+    	eventRepository.delete(event);
+    	return true;
+    }
+    
+    public long count() {
+    	long count = eventRepository.count();
+    	return count;
+    }
 }
