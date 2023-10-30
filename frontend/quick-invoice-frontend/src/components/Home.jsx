9a96e497378ce37336ab816625127cd6999e295d
Shaik Safi <shaiksafi2001@gmail.com>
2023-10-30 21:27:09 +0530

    FE update

--- frontend/quick-invoice-frontend/src/components/Home.jsx
+++ frontend/quick-invoice-frontend/src/components/Home.jsx
@@ -1,7 +1,10 @@
 export default function Home() {
     return (
+      <>
       <h1 className="text-3xl font-bold underline">
         Home 
       </h1>
+      <h2>Welcome to Home Page</h2>
+      </>
     )
   }
